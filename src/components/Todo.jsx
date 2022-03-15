0efac18738e6ae8003a44f51fa12efd19ef4909e
Salman <salman9915189734@gmail.com>
2022-03-16 00:53:52 +0530

    function to add & delete items added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 
 function Todo() {
+    const [inputData, setInputData] = useState("");
+    const [items, setItems] = useState([]);
+    const onChange = (e) => {
+        setInputData(e.target.value)
+    }
+    const addItem = () => {
+        if (!inputData) {
+            alert('pleasse fill the data');
+        } else {
+            const myNewInputData = {
+                id: new Date().getTime().toString(),
+                name: inputData
+            }
+            setItems([...items, myNewInputData])
+            setInputData("")
+        }
+    }
+    const deleteItem = (index) => {
+        const updatedItems = items.filter((item) => {
+            return item.id !== index;
+        });
+        setItems(updatedItems);
+    }
+    const removeAll = () => {
+        setItems([]);
+    }
     return (
         <>
             <div className="main-div">
@@ -11,20 +37,35 @@ function Todo() {
                         <figcaption>Add Your List Here</figcaption>
                     </figure>
                     <div className="addItems">
-                        <input type="text" placeholder=' ✍️ Add Items' className='form-conyrol' />
-                        <i class="fa fa-plus add-btn"></i>
+                        <input
+                            type="text"
+                            placeholder=' ✍️ Add Items'
+                            className='form-conyrol'
+                            value={inputData}
+                            onChange={onChange} />
+                        <i className="fa fa-plus add-btn" onClick={addItem}></i>
                     </div>
                     <div className="showItems">
-                        <div className="eachItem">
-                            <h3>Apple</h3>
-                            <div className="todo-btn">
-                                <i class="far fa-edit"></i>
-                                <i class="far fa-trash-alt"></i>
-                            </div>
-                        </div>
+                        {items.map((item) => {
+                            return (
+                                <div className="eachItem" key={item.id}>
+                                    <h3>{item.name}</h3>
+                                    <div className="todo-btn">
+                                        <i className="far fa-edit"></i>
+                                        <i className="far fa-trash-alt" onClick={() => deleteItem(item.id)}></i>
+                                    </div>
+                                </div>
+                            )
+                        })}
+
                     </div>
                     <div className="showItems">
-                        <button className="btn effect04" data-sm-link-text="Remove All"><span>CHECK LIST</span> </button>
+                        <button
+                            className="btn effect04"
+                            data-sm-link-text="Remove All"
+                            onClick={removeAll} >
+                            <span>CHECK LIST</span>
+                        </button>
                     </div>
                 </div>
             </div>
